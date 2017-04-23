d63741c40891096093c56d2d1577acb267309081
on <olavnybo@gmail.com>
2017-04-23 20:15:11 +0200

    Implemented image upload

---
+++ src/Upload/Controllers/PhotoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace Upload.Controllers
+{
+    [Route("api/photo")]
+    [Authorize(ActiveAuthenticationSchemes = "apikey")]
+    public class PhotoController : Controller
+    {
+        [HttpPost("{userId}")]
+        public async Task<IActionResult> Post(int userId, IFormFile file)
+        {
+
+            if (file == null)
+            {
+                return BadRequest("Action accepts only requests containing a single file");
+            }
+
+            var storageAccount = CloudStorageAccount.Parse(
+                Environment.GetEnvironmentVariable("CUSTOMCONNSTR_EmoStorage")                
+            );
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference("incomingphotos");
+
+            var photoId = Guid.NewGuid();
+            var blockBlob = container.GetBlockBlobReference(photoId.ToString());
+            await blockBlob.UploadFromStreamAsync(file.OpenReadStream());
+
+            return CreatedAtRoute("PhotoLink", new { photoId }, photoId);
+        }
+
+        [HttpGet("{PhotoId}", Name = "PhotoLink")]
+        public async Task<IActionResult> Get(Guid photoId)
+        {
+            
+            return Ok();
+        }
+    }
+}
