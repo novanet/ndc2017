8fa6c2f25db005a574c81f69b947fa7c27327769
Svein Rune Engebakken <sveinrune82@gmail.com>
2017-04-23 19:30:25 +0200

    Replaced DocumentDB-usage in emo-function with API-call

--- src/EmoApi/AzureFunctions/EmotionFunction/run.cs
+++ src/EmoApi/AzureFunctions/EmotionFunction/run.cs
@@ -5,44 +5,60 @@ using System.IO;
 using System;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 
 namespace Novanet {
     public class Emotion {
-        public static async Task<object> Run(Stream myBlob, string name, TraceWriter log)
+        public static async Task Run(Stream myBlob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
-            //Do emotions stuff here
             var emotionsApiKey = Environment.GetEnvironmentVariable("emotionApiKey", EnvironmentVariableTarget.Process);
             var emotionClient = new EmotionServiceClient(emotionsApiKey);
             var emotions = await emotionClient.RecognizeAsync(myBlob);
 
             var blobUri = Environment.GetEnvironmentVariable("storageAccountContainerUri", EnvironmentVariableTarget.Process) + "/" + name;
+            
+            var firstEmotion = emotions.FirstOrDefault();
 
-            var emotionList = emotions.Select(e => new
+            var emotionOutput = new
             {
-                e.Scores.Anger,
-                e.Scores.Contempt,
-                e.Scores.Disgust,
-                e.Scores.Fear,
-                e.Scores.Happiness,
-                e.Scores.Neutral,
-                e.Scores.Sadness,
-                e.Scores.Surprise,
-                e.FaceRectangle
-            }).ToList();
-
-            var outputDocument = new
-            {
-                id = name,
-                FileName = name,
-                BlobUri = blobUri,
-                Emotions = emotionList
+                photoId = name,
+                blobUri = blobUri,
+                anger = firstEmotion.Scores?.Anger ?? 0,
+                contempt = firstEmotion.Scores?.Contempt ?? 0,
+                disgust = firstEmotion.Scores?.Disgust ?? 0,
+                fear = firstEmotion.Scores?.Fear ?? 0,
+                happiness = firstEmotion.Scores?.Happiness ?? 0,
+                neutral = firstEmotion.Scores?.Neutral ?? 0,
+                sadness = firstEmotion.Scores?.Sadness ?? 0,
+                surprise = firstEmotion.Scores?.Surprise ?? 0,
             };
 
-            var converted = JsonConvert.SerializeObject(outputDocument);
+            var converted = JsonConvert.SerializeObject(emotionOutput);
             log.Info(converted);
-            return converted;
+
+            var highscoreApiUri = Environment.GetEnvironmentVariable("highscoreApiPostEmotionsUri", EnvironmentVariableTarget.Process);
+            var result = await PostResult(highscoreApiUri, emotionOutput);
+            log.Info(result);
+        }
+
+        private static async Task<string> PostResult(string uri, dynamic contents)
+        {
+            using (var client = new HttpClient())
+            {
+                var json = JsonConvert.SerializeObject(contents);
+                var requestData = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(uri, requestData);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception($"Sending emotion api result to {uri} failed with httpstatusCode = {response.StatusCode}, reasonPhrase = {response.ReasonPhrase} and content = {result}. Request body was: {json}");
+
+                return result;
+            }
         }
     }
 }
