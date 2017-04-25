dda4c2e3dfbcb56d235cde58ac61d1b70bad9b44
hbrotan <hbrotan@gmail.com>
2017-04-25 13:28:05 +0200

    Only included photos that are stored in dbo.Photo

--- src/Highscore/server.js
+++ src/Highscore/server.js
@@ -50,14 +50,14 @@ router.get('/highscore', function(req, res) {
     sql.execute({
         query: 
 			'SELECT Emotion, [Rank], U.[Name] as [User], Score, P.Id AS [Image] FROM ( ' +
-				'SELECT \'Anger\' AS Emotion, RANK() OVER (ORDER BY Anger DESC) AS [Rank], PhotoId, Anger AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Anger UNION ALL ' +
-				'SELECT \'Contempt\' AS Emotion, RANK() OVER (ORDER BY Contempt DESC) AS [Rank], PhotoId, Contempt AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Contempt UNION ALL ' +
-				'SELECT \'Disgust\' AS Emotion, RANK() OVER (ORDER BY Disgust DESC) AS [Rank], PhotoId, Disgust AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Disgust UNION ALL ' +
-				'SELECT \'Fear\' AS Emotion, RANK() OVER (ORDER BY Fear DESC) AS [Rank], PhotoId, Fear AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Fear UNION ALL ' +
-				'SELECT \'Happiness\' AS Emotion, RANK() OVER (ORDER BY Happiness DESC) AS [Rank], PhotoId, Happiness AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Happiness UNION ALL ' +
-				'SELECT \'Neutral\' AS Emotion, RANK() OVER (ORDER BY Neutral DESC) AS [Rank], PhotoId, Neutral AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Neutral UNION ALL ' +
-				'SELECT \'Sadness\' AS Emotion, RANK() OVER (ORDER BY Sadness DESC) AS [Rank], PhotoId, Sadness AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Sadness UNION ALL ' +
-				'SELECT \'Surprise\' AS Emotion, RANK() OVER (ORDER BY Surprise DESC) AS [Rank], PhotoId, Surprise AS Score FROM dbo.[Emotions] GROUP BY PhotoId, Surprise ' +
+				'SELECT \'Anger\' AS Emotion, RANK() OVER (ORDER BY Anger DESC) AS [Rank], PhotoId, Anger AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Anger UNION ALL ' +
+				'SELECT \'Contempt\' AS Emotion, RANK() OVER (ORDER BY Contempt DESC) AS [Rank], PhotoId, Contempt AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Contempt UNION ALL ' +
+				'SELECT \'Disgust\' AS Emotion, RANK() OVER (ORDER BY Disgust DESC) AS [Rank], PhotoId, Disgust AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Disgust UNION ALL ' +
+				'SELECT \'Fear\' AS Emotion, RANK() OVER (ORDER BY Fear DESC) AS [Rank], PhotoId, Fear AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Fear UNION ALL ' +
+				'SELECT \'Happiness\' AS Emotion, RANK() OVER (ORDER BY Happiness DESC) AS [Rank], PhotoId, Happiness AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Happiness UNION ALL ' +
+				'SELECT \'Neutral\' AS Emotion, RANK() OVER (ORDER BY Neutral DESC) AS [Rank], PhotoId, Neutral AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Neutral UNION ALL ' +
+				'SELECT \'Sadness\' AS Emotion, RANK() OVER (ORDER BY Sadness DESC) AS [Rank], PhotoId, Sadness AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Sadness UNION ALL ' +
+				'SELECT \'Surprise\' AS Emotion, RANK() OVER (ORDER BY Surprise DESC) AS [Rank], PhotoId, Surprise AS Score FROM dbo.[Emotions] WHERE PhotoId IN (SELECT Id FROM dbo.Photo) GROUP BY PhotoId, Surprise ' +
             ') A ' +
             'INNER JOIN dbo.[Photo] P ON P.Id = A.PhotoId ' +
             'INNER JOIN dbo.[User] U ON U.Id = P.UserId ' +
