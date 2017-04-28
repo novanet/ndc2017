5f188e9656338f884c53d33891b97f5ef4c85420
hbrotan <hbrotan@gmail.com>
2017-04-28 11:04:02 +0200

    typo: higestScore -> highestScore

--- src/Highscore/server.js
+++ src/Highscore/server.js
@@ -49,7 +49,7 @@ var router = express.Router();
 router.get('/lastPhoto', function(req, res) {
     sql.execute({
         query: 
-	'SELECT TOP 1 E.*, (SELECT TOP 1 x FROM (VALUES (\'Anger\', Anger), (\'Contempt\', Contempt), (\'Disgust\', Disgust), (\'Fear\', Fear), (\'Happiness\', Happiness), (\'Neutral\', Neutral), (\'Sadness\', Sadness), (\'Surprise\', Surprise)) AS value(x,y) ORDER BY y DESC) as [HigestScore], P.BlobUri, U.[Name]' +
+	'SELECT TOP 1 E.*, (SELECT TOP 1 x FROM (VALUES (\'Anger\', Anger), (\'Contempt\', Contempt), (\'Disgust\', Disgust), (\'Fear\', Fear), (\'Happiness\', Happiness), (\'Neutral\', Neutral), (\'Sadness\', Sadness), (\'Surprise\', Surprise)) AS value(x,y) ORDER BY y DESC) as [HighestScore], P.BlobUri, U.[Name]' +
 	'FROM dbo.[Emotions] E ' + 		
 	'INNER JOIN dbo.[Photo] P ON P.Id = E.PhotoId ' +
 	'INNER JOIN dbo.[User] U ON U.Id = P.UserId ' +            
