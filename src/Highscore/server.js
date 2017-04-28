68c47ff4f49780949d9635d8e8bd1ae2a7d6fee3
hbrotan <hbrotan@gmail.com>
2017-04-28 13:02:58 +0200

    /api/emotions/users/:userId
    All photos with stats for a given user

--- src/Highscore/server.js
+++ src/Highscore/server.js
@@ -88,6 +88,41 @@ router.get('/highscore', function(req, res) {
         throw error;
     });
 });
+router.get('/emotions/users/:userId', function (req, res) {
+	sql.execute({
+		query: 
+			'SELECT * FROM ( ' +
+				'SELECT  U.Id AS UserId, U.NAME AS UserName, ' +
+                        'E.Anger, E.Contempt, E.Disgust, E.Fear, E.Happiness, E.Neutral, E.Sadness, E.Surprise, ' +
+                        '(SELECT TOP 1 x FROM (VALUES (\'Anger\', Anger), (\'Contempt\', Contempt), (\'Disgust\', Disgust), (\'Fear\', Fear), (\'Happiness\', Happiness), (\'Neutral\', Neutral), (\'Sadness\', Sadness), (\'Surprise\', Surprise)) AS value(x,y) ORDER BY y DESC) as [HigestScore],' +
+                        'RANK() OVER (ORDER BY Anger DESC) AS [RankAnger], ' +
+                        'RANK() OVER (ORDER BY Contempt DESC) AS [RankContempt], ' +
+                        'RANK() OVER (ORDER BY Disgust DESC) AS [RankDisgust], ' +
+                        'RANK() OVER (ORDER BY Happiness DESC) AS [RankHappiness], ' +
+                        'RANK() OVER (ORDER BY Neutral DESC) AS [RankNeutral], ' +
+                        'RANK() OVER (ORDER BY Sadness DESC) AS [RankSadness], ' +
+                        'RANK() OVER (ORDER BY Surprise DESC) AS [RankSurprise], ' +
+                        'P.Id AS PhotoId, ' +
+                        'P.BlobUri ' +
+					'FROM dbo.[Emotions] E ' +
+					'INNER JOIN dbo.[Photo] P ON P.Id = E.PhotoId  ' +
+					'INNER JOIN dbo.[User] U ON U.Id = P.UserId ' +
+					'AND LEN(P.BlobUri) > 1  ' +
+				') P ' +
+			'WHERE P.UserId = @userId ',
+		params: {
+			userId: {
+			    type: sql.INT,
+			    val: req.params.userId,
+			}
+		}
+	    }).then(function (results) {
+			res.send(results);
+	    }).catch(function (error) {
+			res.json(error);
+			throw error;
+	    });	
+});
 router.route('/emotions')
     .post(function (req, res) {
         // Add emotions			
