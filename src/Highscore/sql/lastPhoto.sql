cfb2bc25d2924a645f457a4d9d2a8f70f9cf8bcf
hbrotan <hbrotan@gmail.com>
2017-05-12 12:32:28 +0200

    add ranks to last updated photo

--- src/Highscore/sql/lastPhoto.sql
+++ src/Highscore/sql/lastPhoto.sql
@@ -1,26 +1,35 @@
-SELECT TOP 1 E.*, 
-	 P.BlobUri, 
-	 U.[Name],
-	(SELECT TOP 1 x 
-	 FROM (VALUES 
-		('Anger', Anger), 
-		('Contempt', Contempt), 
-		('Disgust', Disgust), 
-		('Fear', Fear), 
-		('Happiness', Happiness), 
-		('Neutral', Neutral), 
-		('Sadness', Sadness), 
-		('Surprise', Surprise)) AS value(x,y)
-	 ORDER BY y DESC) as [HighestScore], 
-	RANK() OVER (ORDER BY Anger DESC) AS [RankAnger],  
-    RANK() OVER (ORDER BY Contempt DESC) AS [RankContempt],  
-    RANK() OVER (ORDER BY Disgust DESC) AS [RankDisgust],  
-    RANK() OVER (ORDER BY Happiness DESC) AS [RankHappiness],  
+SELECT TOP 1
+	P.BlobUri, 
+	U.[Name],
+	E.Anger as 'AngerScore', 
+	E.Contempt as 'ContemptScore', 
+	E.Disgust as 'DisgustScore', 
+	E.Fear as 'FearScore', 
+	E.Happiness as 'HappinessScore', 
+	E.Neutral as 'NeutralScore', 
+	E.Sadness as 'SadnessScore', 
+	E.Surprise as 'SurpriseScore',  
+	RANK() OVER (ORDER BY Anger DESC) AS [RankAnger], 
+    RANK() OVER (ORDER BY Contempt DESC) AS [RankContempt], 
+    RANK() OVER (ORDER BY Disgust DESC) AS [RankDisgust], 
+    RANK() OVER (ORDER BY Fear DESC) AS [RankFear], 
+    RANK() OVER (ORDER BY Happiness DESC) AS [RankHappiness], 
     RANK() OVER (ORDER BY Neutral DESC) AS [RankNeutral],  
-    RANK() OVER (ORDER BY Sadness DESC) AS [RankSadness],  
-    RANK() OVER (ORDER BY Surprise DESC) AS [RankSurprise] 
-FROM dbo.[Emotions] E   		
-INNER JOIN dbo.[Photo] P ON P.Id = E.PhotoId  
-INNER JOIN dbo.[User] U ON U.Id = P.UserId              
+    RANK() OVER (ORDER BY Sadness DESC) AS [RankSadness], 
+    RANK() OVER (ORDER BY Surprise DESC) AS [RankSurprise],
+	(SELECT TOP 1 x 
+		 FROM (VALUES 
+			('Anger', Anger), 
+			('Contempt', Contempt), 
+			('Disgust', Disgust), 
+			('Fear', Fear), 
+			('Happiness', Happiness), 
+			('Neutral', Neutral), 
+			('Sadness', Sadness), 
+			('Surprise', Surprise)) AS value(x,y)
+		 ORDER BY y DESC) as [HighestScore]
+FROM dbo.[Emotions] E 
+INNER JOIN dbo.[Photo] P ON P.Id = E.PhotoId 
+INNER JOIN dbo.[User] U ON U.Id = P.UserId 
 WHERE LEN(P.BlobUri) > 1  
 ORDER BY E.Added DESC  
