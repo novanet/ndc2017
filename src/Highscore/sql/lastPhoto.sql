d8bdb7ceef9a0607d8668b1e4bc0960f39b8e1c0
hbrotan <hbrotan@gmail.com>
2017-05-12 10:54:48 +0200

    add emotion ranks

--- src/Highscore/sql/lastPhoto.sql
+++ src/Highscore/sql/lastPhoto.sql
@@ -1,4 +1,6 @@
 SELECT TOP 1 E.*, 
+	 P.BlobUri, 
+	 U.[Name],
 	(SELECT TOP 1 x 
 	 FROM (VALUES 
 		('Anger', Anger), 
@@ -8,12 +10,17 @@ SELECT TOP 1 E.*,
 		('Happiness', Happiness), 
 		('Neutral', Neutral), 
 		('Sadness', Sadness), 
-		('Surprise', Surprise)) AS value(x,y) 
+		('Surprise', Surprise)) AS value(x,y)
 	 ORDER BY y DESC) as [HighestScore], 
-	 P.BlobUri, 
-	 U.[Name] 
+	RANK() OVER (ORDER BY Anger DESC) AS [RankAnger],  
+    RANK() OVER (ORDER BY Contempt DESC) AS [RankContempt],  
+    RANK() OVER (ORDER BY Disgust DESC) AS [RankDisgust],  
+    RANK() OVER (ORDER BY Happiness DESC) AS [RankHappiness],  
+    RANK() OVER (ORDER BY Neutral DESC) AS [RankNeutral],  
+    RANK() OVER (ORDER BY Sadness DESC) AS [RankSadness],  
+    RANK() OVER (ORDER BY Surprise DESC) AS [RankSurprise] 
 FROM dbo.[Emotions] E   		
 INNER JOIN dbo.[Photo] P ON P.Id = E.PhotoId  
 INNER JOIN dbo.[User] U ON U.Id = P.UserId              
 WHERE LEN(P.BlobUri) > 1  
-ORDER BY E.Added DESC   
+ORDER BY E.Added DESC  
