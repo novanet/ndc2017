3a591a61c42da49dbe695eef801ed951aa0ccf89
Hallstein Brøtan <Hallstein Brtan>
2017-06-14 23:01:40 +0200

    Latest photo is ordered by score

--- src/Highscore/angular.js
+++ src/Highscore/angular.js
@@ -22,7 +22,8 @@
         vm.error = null;
         vm.goToPage = goToPage;
 		vm.highscore = [];
-        vm.isBusyLoading = true;		
+        vm.isBusyLoading = true;
+        vm.latestPhoto = {};	
         vm.slideShow = [];
 		
 		//Init		
@@ -77,7 +78,21 @@
 		function getLatestPhoto(){
 			dataservice.getLatestPhoto()
 				.then(function(result){
-					vm.latestPhoto = result[0];
+                    vm.latestPhoto = {};
+
+                    vm.latestPhoto.blobUri = result[0].BlobUri;
+                    vm.latestPhoto.name = result[0].Name;
+                    vm.latestPhoto.highestScore = result[0].HighestScore;
+
+					vm.latestPhoto.emotions = [];
+                    vm.latestPhoto.emotions.push({'name': 'Anger', 'score': result[0].AngerScore, 'rank': result[0].RankAnger });
+                    vm.latestPhoto.emotions.push({'name': 'Contempt', 'score': result[0].ContemptScore, 'rank': result[0].RankContempt });
+                    vm.latestPhoto.emotions.push({'name': 'Disgust', 'score': result[0].DisgustScore, 'rank': result[0].RankDisgust });
+                    vm.latestPhoto.emotions.push({'name': 'Fear', 'score': result[0].FearScore, 'rank': result[0].RankFear });
+                    vm.latestPhoto.emotions.push({'name': 'Happiness', 'score': result[0].HappinessScore, 'rank': result[0].RankHappiness });
+                    vm.latestPhoto.emotions.push({'name': 'Neutral', 'score': result[0].NeutralScore, 'rank': result[0].RankNeutral });
+                    vm.latestPhoto.emotions.push({'name': 'Sadness', 'score': result[0].SadnessScore, 'rank': result[0].RankSadness });
+                    vm.latestPhoto.emotions.push({'name': 'Surprise', 'score': result[0].SurpriseScore, 'rank': result[0].RankSurprise });
 				});
 		}
     }
